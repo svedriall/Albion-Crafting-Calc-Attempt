5146352eb08109c4ff1626915a012cb77e0175d8
Anil Karagoz <svedriall@gmail.com>
2019-06-11 00:31:51 +0300

    Home to School Day 2

--- Food/mysite/migrater.py
+++ Food/mysite/migrater.py
@@ -31,15 +31,31 @@ def Req_Pricer(item_id, level):
         resc_price = int(mydata[0]['sell_price_min'])
         resc_count = int(level.get('count'))
         resc_total = resc_count * resc_price
-    return resc_price, resc _total;
+    return resc_price, resc_total;
+
+def Req_Namer(item_id):
+    if item_id.find('_LEVEL1') != -1:
+        item_id = item_id.replace('_LEVEL1', '')
+    elif item_id.find('_LEVEL2') != -1:
+        item_id = item_id.replace('_LEVEL2', '')
+    elif uniqueid.find('_LEVEL3') != -1:
+        item_id = item_id.replace('_LEVEL3', '')
+    page = "https://gameinfo.albiononline.com/api/gameinfo/items/"+item_id+"/data"
+    file = urllib.request.urlopen(page)
+    data = file.read()
+    mydata = json.loads(data)
+
+    resc_itemname = json.dumps(mydata['localizedNames']['EN-US'])
+    return resc_itemname;
 
 
 # list = ['consumableitem', 'farmableitem', 'simpleitem', 'consumablefrominventoryitem', 'equipmentitem', 'weapon', 'mount', 'furnitureitem', 'journalitem']
-list = ['consumableitem']
+list = ['weapon']
 
 # for tags in root:
 #     if tags.tag not in list:
 #         list.append(tags.tag)
+
 for iterate in list:
     for consumableitem in root.findall(iterate):
         uniqueid = consumableitem.get('uniquename')
@@ -49,7 +65,7 @@ for iterate in list:
         shopsubcategory = consumableitem.get('shopsubcategory1')
         slottype = consumableitem.get('slottype')
         nutrition = consumableitem.get('nutrition')
-
+        item_name = Req_Namer(uniqueid)
         price = Pricer(uniqueid)
         print("- ID:", uniqueid,
               "- Tier:", tier,
@@ -58,21 +74,26 @@ for iterate in list:
               "- Cat:", shopcategory,
               "- Subcat:", shopsubcategory,
               "- Price: ", price,
-              "- Nutrition: ", nutrition,)
+              "- Nutrition: ", nutrition,
+              "- Name: ", item_name)
 
         # CRAFTING REQUIREMENTS #
         for craftingrequirements in consumableitem.findall('craftingrequirements'):
 
             amountcrafted = craftingrequirements.get('amountcrafted')
+            if amountcrafted is None:
+                amountcrafted == 1
             craftingfocus = craftingrequirements.get('craftingfocus')
 
             print("- FCS:", craftingfocus, 'Crft Amt:', amountcrafted)
             for reqCrafts in craftingrequirements.findall('craftresource'):
-                req_name = reqCrafts.get('uniquename')
+                req_id = reqCrafts.get('uniquename')
                 req_count = int(reqCrafts.get('count'))
                 maxreturnamount = reqCrafts.get('maxreturnamount')
-
-                total = Req_Pricer(req_name, reqCrafts)
+                if maxreturnamount is None:
+                    maxreturnamount == int(0)
+                req_name = Req_Namer(req_id)
+                total = Req_Pricer(req_name, req_id)
 
                 print("->", "ID:", req_name, "Count:", req_count, "Total Price:", total, "MaxReturn: ", maxreturnamount)
         # ENCHANTED ITEM DETAILS #
@@ -82,11 +103,12 @@ for iterate in list:
                 # CRAFT SOURCES #
                 for craftreq in enchantment.findall('craftingrequirements'):
                     for craftresource in craftreq.findall('craftresource'):
-                        resc_name = craftresource.get('uniquename')
+                        resc_id = craftresource.get('uniquename')
                         resc_count2 = craftresource.get('count')
-
-                        total2 = Req_Pricer(resc_name, craftresource)
+                        resc_name = Req_Namer(resc_id)
+                        total2 = Req_Pricer(resc_id, craftresource)
 
                         print("->", "Resc ID:", resc_name, "- Count:", resc_count2, '- Total:', total2)
         print("\n")
 print(list)
+print("######### SUCCESS #########")
--- Food/mysite/puller.py
+++ Food/mysite/puller.py
@@ -1,14 +1,29 @@
 import json
 import urllib
 from urllib.request import Request, urlopen
+resc_itemdescription = ""
+def req_namer(uniqueid):
+    if uniqueid.find('_LEVEL1') != -1:
+        uniqueid = uniqueid.replace('_LEVEL1', '')
+    elif uniqueid.find('_LEVEL2') != -1:
+        uniqueid = uniqueid.replace('_LEVEL2', '')
+    elif uniqueid.find('_LEVEL3') != -1:
+        uniqueid = uniqueid.replace('_LEVEL3', '')
+    page = "https://gameinfo.albiononline.com/api/gameinfo/items/"+uniqueid+"/data"
+    file = urllib.request.urlopen(page)
+    data = file.read()
+    mydata = json.loads(data)
 
-uniqueid = "T8_MEAL_STEW"
-page = "https://www.albion-online-data.com/api/v2/stats/prices/" + uniqueid + "?locations=Caerleon&qualities=0"
-file = urllib.request.urlopen(page)
-data = file.read()
-mydata = json.loads(data)
+    resc_itemname = json.dumps(mydata['localizedNames']['EN-US'])
+    resc_itemdescription = json.dumps(mydata['localizedDescriptions']['EN-US'])
+    return resc_itemname, resc_itemdescription;
 
-print(json.dumps(mydata))
+uniqueid = "T1_FISH_FRESHWATER_ALL_COMMON"
+# uniqueid = "T8_MEAL_STEW"
+x, y = req_namer(uniqueid)
+print(x, y)
+
+
 
 # for t in range(len(mydata)):
 #    print(mydata[0]['buy_price_max'])

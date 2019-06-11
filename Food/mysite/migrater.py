ca06e370632abe32678e54b1e75078864de2bcdd
Anil Karagoz <svedriall@gmail.com>
2019-06-12 01:41:40 +0300

    Home to School 3
    first db imports. gotta change the codes to be able to import into DB

--- Food/mysite/migrater.py
+++ Food/mysite/migrater.py
@@ -3,6 +3,16 @@ import json
 import urllib
 from urllib.request import Request
 from urllib.error import HTTPError
+import sqlite3
+conn = sqlite3.connect('AO.db')
+
+c = conn.cursor();
+
+# ITERATE HERE
+
+
+# END ITERATE
+
 
 parser = ET.XMLParser(encoding="utf-8")
 
@@ -14,6 +24,7 @@ failed_names_500_list = []
 failed_names_localized_list = []
 
 def Pricer(item_id):
+    global price
     try:
         page = "https://www.albion-online-data.com/api/v2/stats/prices/" + item_id + "?locations=Caerleon&qualities=0"
         file = urllib.request.urlopen(page)
@@ -22,7 +33,6 @@ def Pricer(item_id):
         if mydata[0]['sell_price_min'] is not None:
             for t in range(len(mydata)):
                 price = int(mydata[0]['sell_price_min'])
-                print("success")
         else:
             failed_prices_list.append(item_id)
             price = "Failed - Saved"
@@ -42,7 +52,7 @@ def Req_Pricer(item_id, level):
         file = urllib.request.urlopen(page)
         data = file.read()
         mydata = json.loads(data)
-        if mydata[0]['sell_price_min']
+        if mydata[0]['sell_price_min']:
             for t in range(len(mydata)):
                 try:
                     resc_price = int(mydata[0]['sell_price_min'])
@@ -54,12 +64,11 @@ def Req_Pricer(item_id, level):
                     print(item_id + "failed to catch price")
         else:
             failed_prices_list.append(item_id)
-            price = "Failed - Saved"
-                print(item_id, " !!!!! failed to get price !!!!!")
+            resc_price = "Failed - Saved"
+            print(item_id, " !!!!! failed to get price !!!!!")
     except HTTPError:
         resc_price = 101010
         resc_total = 101010
-
     return resc_price, resc_total;
 
 
@@ -96,34 +105,36 @@ list = ['consumableitem', 'farmableitem', 'simpleitem', 'consumablefrominventory
 for iterate in list:
     for item in root.findall(iterate):
         #### MAIN ITEM INFO #####
-        uniqueid = item.get('uniquename')
+        uniqueid = str(item.get('uniquename'))
+        temp_id = str(item.get('uniquename'))
         tier = item.get('tier')
         weight = item.get('weight')
         shopcategory = item.get('shopcategory')
         shopsubcategory = item.get('shopsubcategory1')
         slottype = item.get('slottype')
         #### WHAT TO SUBCAT ####
-        if shopsubcategory == "fish":
-            item_name = Namer(uniqueid)
-            price = Pricer(uniqueid)
+        if shopsubcategory == "cooked":
+            # price = Pricer(uniqueid)
 
             nutrition = item.get('nutrition')
             if nutrition is None:
                 nutrition = "Not Food"
-            if item_name == "500 ERROR - WRONG ID":
-                item_name = uniqueid
 
-            print("####   " + item_name + "   ####")
-            print("| ID:", uniqueid,
-                  "| Price: ", price,
-                  "| Tier:", tier)
+            # item_name = Namer(uniqueid)
+            # if item_name == "500 ERROR - WRONG ID":
+            #     item_name = uniqueid
 
-            print("| Slot:", slottype,
-                  "| Cat:", shopcategory,
-                  "| Subcat:", shopsubcategory)
-
-            print("| Nutrition: ", nutrition,
-                  "| Weight:", weight, )
+            # print("####   " + item_name + "   ####")
+            # print("| ID:", uniqueid,
+            #       "| Price: ", price,
+            #       "| Tier:", tier)
+            #
+            # print("| Slot:", slottype,
+            #       "| Cat:", shopcategory,
+            #       "| Subcat:", shopsubcategory)
+            #
+            # print("| Nutrition: ", nutrition,
+            #       "| Weight:", weight, )
 
             # CRAFTING REQUIREMENTS #
             for craftingrequirements in item.findall('craftingrequirements'):
@@ -136,8 +147,8 @@ for iterate in list:
                 if craftingfocus is None:
                     craftingfocus = "Not Craftable"
 
-                print("| Crft. Focus:", craftingfocus, '- Crafted Amount:', amountcrafted)
-
+                # print("| Crft. Focus:", craftingfocus, '- Crafted Amount:', amountcrafted)
+                req_main_list = []
                 for CraftingRequirements_Attrib in craftingrequirements.findall('craftresource'):
                     req_main_id = CraftingRequirements_Attrib.get('uniquename')
                     req_main_count = int(CraftingRequirements_Attrib.get('count'))
@@ -145,28 +156,48 @@ for iterate in list:
                     maxreturnamount = CraftingRequirements_Attrib.get('maxreturnamount')
                     if maxreturnamount is None:
                         maxreturnamount = "0"
+                    # req_main_name = Namer(req_main_id)
+                    req_main_list.append(req_main_id)
 
-                    req_main_name = Namer(req_main_id)
-                    total = Req_Pricer(req_main_id, CraftingRequirements_Attrib)
-                    print("->", "ID:", req_main_name, "Count:", req_main_count, "Total Price:", total, "MaxReturn: ",
-                          maxreturnamount)
-
+                    # total = Req_Pricer(req_main_id, CraftingRequirements_Attrib)
+                    # print("->", "ID:", req_main_name, "Count:", req_main_count, "Total Price:", total, "MaxReturn: ",maxreturnamount)
             # ENCHANTED ITEM DETAILS #
+            newlist = []
+            newlist = str(",".join(req_main_list))
+            print(uniqueid,":",newlist)
+            c.execute("INSERT INTO Components VALUES ('" + uniqueid + "','" + newlist + "')");
+            # x = "INSERT INTO Components VALUES ('" + uniqueid + ",'" + newlist + "')"
+            # print(x)
+            # c.execute(x);
             for enchantments in item.findall('enchantments'):
                 for enchantment in enchantments.findall('enchantment'):
-                    print("ID:", uniqueid, "Enchant:", enchantment.get('enchantmentlevel'))
+                    enchantmentlevel = enchantment.get('enchantmentlevel')
+                    uniqueid = temp_id + "@" + enchantmentlevel
+                    # print("ID:", uniqueid, "Enchant:", enchantment.get('enchantmentlevel'))
                     # CRAFT SOURCES #
                     for craftreq in enchantment.findall('craftingrequirements'):
+                        req_main_list = []
+                        newlist = []
                         for craftresource in craftreq.findall('craftresource'):
+
                             resc_id = craftresource.get('uniquename')
                             resc_count2 = craftresource.get('count')
-                            resc_name = Namer(resc_id)
-                            total2 = Req_Pricer(resc_id, craftresource)
+                            req_main_list.append(resc_id)
 
-                            print("->", "Resc ID:", resc_name, "- Count:", resc_count2, '- Total:', total2)
-            print("\n")
+                            # resc_name = Namer(resc_id)
+                            # total2 = Req_Pricer(resc_id, craftresource)
+
+                            # print("->", "Resc ID:", resc_id, "- Count:", resc_count2, '- Total:')
+
+                        newlist = str(",".join(req_main_list))
+                        print(uniqueid, ":", newlist)
+                        c.execute("INSERT INTO Components VALUES ('" + uniqueid + "','" + newlist + "')");
+            # print("\n")
 # print(list)
 print("######### SUCCESS #########")
-print(*failed_names_list, sep=", ")
+print(*failed_names_localized_list, sep=", ")
 # print("\n")
 print(*failed_prices_list, sep=", ")
+
+conn.commit();
+conn.close();
